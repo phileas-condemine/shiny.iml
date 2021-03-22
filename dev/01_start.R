cf9952f633b6ca0233fa33bdd667703a76416a5a
Philéas Condemine <phileas.condemine@sante.gouv.fr>
2021-03-22 12:58:48 +0100

    Initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,59 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+golem::fill_desc(
+  pkg_name = "shiny.iml", # The Name of the package containing the App 
+  pkg_title = "Shiny_IML", # The Title of the package containing the App 
+  pkg_description = "Interpretable Machine Learning Shiny App.", # The Description of the package containing the App 
+  author_first_name = "Phileas", # Your First Name
+  author_last_name = "Condemine", # Your Last Name
+  author_email = "phileas.condemine@gmail.com", # Your Email
+  repo_url = "https://github.com/phileas-condemine/shiny.iml" # The URL of the GitHub Repo (optional) 
+)     
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license( name = "Phileas Condemine" )  # You can set another license here
+usethis::use_readme_rmd( open = TRUE )
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::remove_favicon()
+golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
